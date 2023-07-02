f2712d76c0a4614e95e2be667600463c5c08e4f5
John Paul Fernandez <paul.fernandez@gamesysgroup.com>
2023-07-02 20:58:27 +0800

    dev-001/1/1: Update css file structure

--- playground_0_vite/src/main.tsx
+++ playground_0_vite/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import "./css/tailwind.css";
+// import "./css/tailwind.css";
 // import 'bootstrap/dist/css/bootstrap.css'
 import "./css/style.css";
 
