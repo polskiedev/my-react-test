5ab1e377a28199dce88214f54981d9caa927afa0
John Paul Fernandez <paul.fernandez@gamesysgroup.com>
2023-07-06 02:50:48 +0800

    dev-001/1/1: Update game list skeleton

--- playground_0_vite/src/components/Skeleton/GameTileSkeleton.tsx
+++ playground_0_vite/src/components/Skeleton/GameTileSkeleton.tsx
@@ -4,15 +4,19 @@ import Skeleton from 'react-loading-skeleton';
 const GameTileSkeleton = ({ count = 6 }) => {
   return (
     <React.Fragment>
-      <div className="flex flex-row gap-1 justify-between items-center mx-5 lg:m-0 p-2">
-        <Skeleton height={35} width={180}/>
+      <div className="flex flex-row gap-1 justify-between items-center mx-5 lg:m-0 py-2">
+        <Skeleton height={35} width={200}/>
       </div>
       <div className="flex flex-row gap-1">
-      {Array(count).fill(0).map((_, i) => (
-        <div>
-          <Skeleton width={150} height={150} />
+        <div className="carousel group relative mx-5 lg:mx-0">
+          <div className="flex flex-row gap-1 overflow-hidden">
+            {Array(count).fill(0).map((_, i) => (
+              <div>
+                <Skeleton width={150} height={150} />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
       </div>
     </React.Fragment>
   );
