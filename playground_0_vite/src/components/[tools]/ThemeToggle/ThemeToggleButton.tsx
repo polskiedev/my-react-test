fe6a221555afc81c50a28a40db75c90e0976a95c
John Paul Fernandez <paul.fernandez@gamesysgroup.com>
2023-07-30 22:18:09 +0800

    DEV-001/1/1: Put keyframe animations to base style

--- playground_0_vite/src/components/[tools]/ThemeToggle/ThemeToggleButton.tsx
+++ playground_0_vite/src/components/[tools]/ThemeToggle/ThemeToggleButton.tsx
@@ -59,8 +59,8 @@ const ThemeToggleButton: React.FC = () => {
         <div className="controls flex flex-row justify-end">
         {toggleButtons ? (
           <button onClick={handleToggleButtons}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </button>
         ) : (
