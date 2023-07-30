f433bd6a3d9af808c7b4fcfe4a4b9af2156498e8
John Paul Fernandez <paul.fernandez@gamesysgroup.com>
2023-07-30 21:42:39 +0800

    DEV-001/1/1: fix bug

--- playground_0_vite/src/components/[tools]/ThemeToggle/ThemeToggleButton.tsx
+++ playground_0_vite/src/components/[tools]/ThemeToggle/ThemeToggleButton.tsx
@@ -56,7 +56,7 @@ const ThemeToggleButton: React.FC = () => {
   return (
     <>
       <div className={`${mainCB.build()} ${toggleComponent}`}>
-        <div className="controls flex flex-row justify-end ${toggleComponent">
+        <div className="controls flex flex-row justify-end">
         {toggleButtons ? (
           <button onClick={handleToggleButtons}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
